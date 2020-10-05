6998b625fd9b091c83033ee83703e8cc9ff7a8e4
Tyler Dolph <tdolph0016@myduneland.org>
2020-10-05 09:26:26 -0500

    added multiplication, fixed comments

--- Fraction.java
+++ Fraction.java
@@ -1,25 +1,32 @@
 
 /**
- * Write a description of class Fraction here.
+ * defines a fraction and has methods for fraction arithmetic
  *
- * @author (your name)
- * @version (a version number or a date)
+ * Tyler Dolph, Brian Hansen
+ * 10/5/2020
  */
 public class Fraction
 {
-    // instance variables - replace the example below with your own
+    //instance variables 
     private int n;
     private int d;
 
-    public Fraction(int num,int den){
+    public Fraction(int num,int den){ //constructor
         n = num; 
         d = den;
     }
     
-    public String ToString(){return(n+"/"+d);}
+    public String ToString(){return(n+"/"+d);} //tostring and accessors
+    public int Numerator(){return(n);}
+    public int Denominator(){return(d);}
     
-    public void Add(int n2, int d2){
+    public void Add(int n2, int d2){ //addition
         n = (n*d2)+(n2*d);
         d = (d*d2);
+    }
+    
+    public void Multiply(int n2, int d2){ //multiplication
+        n = n*n2;
+        d = d*d2;
     }
 }
